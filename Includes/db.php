7ac25e87d0e65b29086accb633830df69839cf5c
Esra Nur Sen <esranurssen@gmail.com>
2026-05-13 14:52:02 +0300

    Initial commit: Established database connection and basic styling

---
+++ Includes/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "portfolio_db";
+
+try{
+    $pdo = new PDO ("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e){
+    echo "Connection failed: " . $e -> getMessage();
+}
+?>
